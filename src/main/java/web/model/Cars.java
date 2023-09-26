04517cdd3a5e9e8ade7551d9734ba33555ec7731
Ivan Nikolaev <ivanix8@gmail.com>
2023-09-26 21:01:14 +0400

    1

--- src/main/java/web/model/Cars.java
+++ src/main/java/web/model/Cars.java
@@ -1,4 +1,34 @@
 package web.model;
 
 public class Cars {
+    private int id;
+    private String name;
+    public Cars(){}
+
+
+    public Cars(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Im a Car";
+    }
 }
