4238ed776cfae51f26f091cbef57e2afd6f04ec0
Ivan Nikolaev <ivanix8@gmail.com>
2023-09-26 21:04:19 +0400

    1

--- src/main/java/web/model/Cars.java
+++ src/main/java/web/model/Cars.java
@@ -3,12 +3,24 @@ package web.model;
 public class Cars {
     private int id;
     private String name;
+
+    private int weight;
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
     public Cars(){}
 
 
-    public Cars(int id, String name) {
+    public Cars(int id, String name, int weight) {
         this.id = id;
         this.name = name;
+        this.weight = weight;
     }
 
     public void setId(int id) {
