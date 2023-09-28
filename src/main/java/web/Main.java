fc38d66b591289ae7ee68e0ba2b762cc80b2d7ee
Ivan Nikolaev <ivanix8@gmail.com>
2023-09-28 18:24:27 +0400

    1

---
+++ src/main/java/web/Main.java
@@ -0,0 +1,6 @@
+package web;
+
+public class Main {
+    public static void main(String[] args){
+    }
+}
---
+++ src/main/java/web/dao/CarsDao.java
@@ -0,0 +1,34 @@
+package web.dao;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import web.model.Cars;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class CarsDao {
+    private static int Cars_count;
+    private List<Cars> carsList;
+    {
+        carsList = new ArrayList<>();
+        carsList.add(new Cars(++Cars_count, "Lada", 1000));
+        carsList.add(new Cars(++Cars_count, "Niva", 1200));
+        carsList.add(new Cars(++Cars_count, "Mercedes", 1300));
+        carsList.add(new Cars(++Cars_count, "UAZ", 2000));
+        carsList.add(new Cars(++Cars_count, "Gazel", 2500));
+    }
+
+    public List<Cars> getCarsList() {
+        return carsList;
+    }
+    public List<Cars> getCountCars(int count){
+        List <Cars> countCars = new ArrayList<>();
+        for(int i = 0; i<count; i++){
+            countCars.add(carsList.get(i));
+        }
+return countCars;
+        }
+    }
+
+
